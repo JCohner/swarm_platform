dfdfbd1ccefdad7521ca4998cfa42fc1fe834fd3
JCohner <joshuacohen2019@u.northwestern.edu>
2020-03-14 19:16:33 -0500

    reworking deeply

--- zumo_rf.c
+++ zumo_rf.c
@@ -103,31 +103,31 @@ void rf_setup()
     /* Customize the CMD_PROP_TX command for this application */
    RF_cmdPropTx.pktLen = PAYLOAD_LENGTH;
    RF_cmdPropTx.pPkt = packet;
-   RF_cmdPropTx.startTrigger.triggerType = TRIG_ABSTIME;
+   RF_cmdPropTx.startTrigger.triggerType = TRIG_NOW;
    RF_cmdPropTx.startTrigger.pastTrig = 1; //if you miss a trigger, trigger as soon as possible
-   RF_cmdPropTx.startTime = 0; //start radio timer at 0
+//   RF_cmdPropTx.startTime = 0; //start radio timer at 0
 //   //my addition
 //   RF_cmdPropTx.startTrigger.triggerType = TRIG_REL_PREVEND;
 //   RF_cmdPropTx.startTime = TX_DELAY;
 
-//   /* Modify CMD_PROP_TX and CMD_PROP_RX commands for application needs */
-//   /* Set the Data Entity queue for received data */
-//   RF_cmdPropRxSniff.pQueue = &dataQueue;
-//   /* Discard ignored packets from Rx queue */
-//   RF_cmdPropRxSniff.rxConf.bAutoFlushIgnored = 1;
-//   /* Discard packets with CRC error from Rx queue */
-//   RF_cmdPropRxSniff.rxConf.bAutoFlushCrcErr = 1;
-//   /* Implement packet length filtering to avoid PROP_ERROR_RXBUF */
-//   RF_cmdPropRxSniff.maxPktLen = PAYLOAD_LENGTH;
-//   /* End RX operation when a packet is received correctly and move on to the
-//    * next command in the chain */
-//   RF_cmdPropRxSniff.pktConf.bRepeatOk = 0;
-//   RF_cmdPropRxSniff.pktConf.bRepeatNok = 1; //just keeps listening after failed receive
-//   RF_cmdPropRxSniff.startTrigger.triggerType = TRIG_NOW;
-//   //RF_cmdPropRxSniff.pNextOp = (rfc_radioOp_t *)&RF_cmdPropTx;
-//   /* Only run the TX command if RX is successful */
-//   RF_cmdPropRxSniff.condition.rule = COND_STOP_ON_FALSE;
-//   RF_cmdPropRxSniff.pOutput = (uint8_t *)&rxStatistics;
+   /* Modify CMD_PROP_TX and CMD_PROP_RX commands for application needs */
+   /* Set the Data Entity queue for received data */
+   RF_cmdPropRxSniff.pQueue = &dataQueue;
+   /* Discard ignored packets from Rx queue */
+   RF_cmdPropRxSniff.rxConf.bAutoFlushIgnored = 1;
+   /* Discard packets with CRC error from Rx queue */
+   RF_cmdPropRxSniff.rxConf.bAutoFlushCrcErr = 1;
+   /* Implement packet length filtering to avoid PROP_ERROR_RXBUF */
+   RF_cmdPropRxSniff.maxPktLen = PAYLOAD_LENGTH;
+   /* End RX operation when a packet is received correctly and move on to the
+    * next command in the chain */
+   RF_cmdPropRxSniff.pktConf.bRepeatOk = 0;
+   RF_cmdPropRxSniff.pktConf.bRepeatNok = 1; //just keeps listening after failed receive
+   RF_cmdPropRxSniff.startTrigger.triggerType = TRIG_NOW;
+   //RF_cmdPropRxSniff.pNextOp = (rfc_radioOp_t *)&RF_cmdPropTx;
+   /* Only run the TX command if RX is successful */
+   RF_cmdPropRxSniff.condition.rule = COND_STOP_ON_FALSE;
+   RF_cmdPropRxSniff.pOutput = (uint8_t *)&rxStatistics;
 //
    /* Modify CMD_PROP_TX and CMD_PROP_RX commands for application needs */
       /* Set the Data Entity queue for received data */
@@ -168,6 +168,7 @@ void rf_setup()
 
 }
 uint32_t curtime; //get cur time
+char buffer[50];
 #define PACKET_INTERVAL     (uint32_t)(4000000*1.0f)
 void rf_main()
 {
@@ -185,9 +186,15 @@ void rf_main()
 //              &sniff_callback, (RF_EventCmdDone |
 //                      RF_EventLastCmdDone | RF_EventRxEntryDone | RF_EventRxOk));
 
-    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropRx, RF_PriorityNormal,
-                  &sniff_callback, (RF_EventCmdDone |
+//    RF_CmdHandle rxCommandHandle =
+            RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropRxSniff, RF_PriorityNormal,
+                       &sniff_callback, (RF_EventCmdDone |
                           RF_EventLastCmdDone | RF_EventRxEntryDone | RF_EventRxOk));
+//    if (rxCommandHandle < 0)
+//    {
+//        WriteUART0("ahh shiiit\r\n");
+//    }
+
     if (curr_time > wait_time && !(wait_time < 0))
     {
 
@@ -197,13 +204,42 @@ void rf_main()
     idle_count = (idle_count + 1) % 14; //just for deugging rx without carrier sense complexities
     if((idle_count > 12) && (wait_time < 0))
     {
-        WriteUART0("hiiii\r\n");
+        WriteUART0("hiiii you\r\n");
         idle_count = 0;
-        RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropTx, RF_PriorityNormal,
-                  &sneeze_callback, (RF_EventCmdDone | RF_EventLastCmdDone));
+        RF_EventMask terminationReason =
+                RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropTx, RF_PriorityNormal,
+                  &sneeze_callback,(RF_EventCmdDone | RF_EventLastCmdDone));
+
+        switch(terminationReason)
+                {
+                    case RF_EventLastCmdDone:
+                        // A stand-alone radio operation command or the last radio
+                        // operation command in a chain finished.
+                        break;
+                    case RF_EventCmdCancelled:
+                        // Command cancelled before it was started; it can be caused
+                    // by RF_cancelCmd() or RF_flushCmd().
+                        break;
+                    case RF_EventCmdAborted:
+                        // Abrupt command termination caused by RF_cancelCmd() or
+                        // RF_flushCmd().
+                        break;
+                    case RF_EventCmdStopped:
+                        // Graceful command termination caused by RF_cancelCmd() or
+                        // RF_flushCmd().
+                        break;
+                    default:
+                        // Uncaught error event
+                        sprintf(buffer, "binch we went uncaught my guy: %lX\r\n", (uint64_t)terminationReason);
+                        WriteUART0(buffer);
+                        while(1);
+                }
+
+
     }
 
 }
+
 
 void sneeze_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
 {
@@ -214,10 +250,57 @@ void sneeze_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
         WriteUART0("shouting\r\n");
     }
 
+    sprintf(buffer, "sniff status code is: %X\r\n", (RF_cmdPropTx.status & 0x0000FFFF));
+    WriteUART0(buffer);
+
+
 }
 
-char buffer[50];
-
+//void sniff_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+//{
+////    GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
+//
+////    if ((RF_cmdPropCs.status == PROP_DONE_BUSY) || (RF_cmdPropCs.status == PROP_DONE_BUSYTIMEOUT))
+////    {
+////        message_time = RF_getCurrentTime();
+////        delta_message_time = (message_time - prev_message_time) * 3;
+////        prev_message_time = message_time;
+////        GPIO_toggleDio(CC1310_LAUNCHXL_PIN_RLED);
+////    }
+//
+//    if(RF_cmdPropRx.status == PROP_DONE_IDLE)
+//    {
+//        GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
+////        WriteUART0("theres no one out there :(\r\n");
+//        idle_count++;
+//    }
+//
+//    sprintf(buffer, "sniff status code is: %X\r\n", (RF_cmdPropRx.status & 0x0000FFFF));
+//    WriteUART0(buffer);
+//
+//    if (e & RF_EventRxEntryDone || RF_cmdPropRx.status == PROP_DONE_OK)
+//    {
+//        message_time = RF_getCurrentTime();
+//        delta_message_time = (message_time - prev_message_time) * 3;
+//        prev_message_time = message_time;
+//        /* Get current unhandled data entry */
+//        currentDataEntry = RFQueue_getDataEntry();
+//
+//        /* Handle the packet data, located at &currentDataEntry->data:
+//         * - Length is the first byte with the current configuration
+//         * - Data starts from the second byte */
+//        packetLength      = *(uint8_t *)(&(currentDataEntry->data));
+//        packetDataPointer = (uint8_t *)(&(currentDataEntry->data) + 1);
+//        RFQueue_nextEntry();
+//
+//        WriteUART0("I heard from someone halejewya!\r\n");
+//
+////        GPIO_toggleDio(CC1310_LAUNCHXL_PIN_RLED);
+//    }
+//
+//
+////    WriteUART0("call back made binch\r\n");
+//}
 void sniff_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
 {
 //    GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
