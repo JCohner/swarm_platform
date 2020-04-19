39b85da6f41bf807ed5320851f062e91c367eaa3
JCohner <joshuacohen2019@u.northwestern.edu>
2020-04-18 21:07:53 -0500

    got a singleshot adc pin read working, the READADC() function doesnt quite work yet, but the singular read in init does

--- cc1310_code/adc.c
+++ cc1310_code/adc.c
@@ -11,22 +11,28 @@ char buffer[50];
 
 void InitADC()
 {
+    AONWUCAuxWakeupEvent(AONWUC_AUX_WAKEUP);
+    while(!(AONWUCPowerStatusGet() & AONWUC_AUX_POWER_ON));
+
     AUXWUCPowerCtrl(AUX_WUC_POWER_ACTIVE);
     //eneable interface and adc clock
-    AUXWUCClockEnable(AUX_WUC_ADC_CLOCK);
+    AUXWUCClockEnable(AUX_WUC_ADC_CLOCK | AUX_WUC_ANAIF_CLOCK | AUX_WUC_ADI_CLOCK);
     //wait for request ack
     while (AUXWUCClockStatus(AUX_WUC_ADC_CLOCK) != AUX_WUC_CLOCK_READY);
 
-    AUXADCSelectInput(ADC_COMPB_IN_AUXIO2);
+    AUXADCSelectInput(ADC_COMPB_IN_AUXIO7); //CORRESPONDS TO DIO PIN 23
+    WriteUART0("sizzurp\r\n");
     //set input pins
-    IOCPinTypeAux(IOID_25); //TODO: figure out how to map pins better
-
-
-    //TODO HAVAE TO MAKE ENABLE CALL BEFORE I DISABLE
-    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_5P3_US, AUXADC_TRIGGER_MANUAL);
+//    IOCPinTypeAux(IOID_25); //TODO: figure out how to map pins better
+    AUXADCDisable();
+    WriteUART0("rock chalk?\r\n");
+    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_2P7_US, AUXADC_TRIGGER_MANUAL);
     AUXADCGenManualTrigger();
-    AUXADCReadFifo();
-    //configure ADC registers
+    WriteUART0("killed jayhawk?\r\n");
+    uint16_t sing_samp = AUXADCReadFifo();
+    sprintf(buffer, "adc val: %u\r\n", sing_samp);
+    WriteUART0(buffer);
+//    //configure ADC registers
     AUXADCDisable();
 //    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_5P3_US, AUXADC_TRIGGER_MANUAL);
     WriteUART0("am i hancocking here?\r\n");
@@ -34,6 +40,10 @@ void InitADC()
 
 uint32_t ReadADC()
 {
+    AUXADCDisable();
+    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_2P7_US, AUXADC_TRIGGER_MANUAL);
     AUXADCGenManualTrigger();
+    uint16_t sing_samp = AUXADCReadFifo();
+    AUXADCDisable();
     return AUXADCReadFifo();
 }
