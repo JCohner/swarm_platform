d6646555a227204510436c344aed89c558d87f31
JCohner <joshuacohen2019@u.northwestern.edu>
2020-05-10 18:34:14 -0500

    added more utils for clock management

--- cc1310_code/helpful.c
+++ cc1310_code/helpful.c
@@ -9,6 +9,7 @@
 #include "uart.h"
 
 char buffer[50];
+
 void delay(float sec)
 {
     uint32_t num_ticks = (uint32_t) (sec * 4800000); //weird behavior: seems to work when clk freq is 4.8 * 10^6 not 48 * 10^6
@@ -25,3 +26,31 @@ void delay(float sec)
 
 }
 
+void EnableTimer()
+{
+    //enable peripheral
+   PRCMPeripheralRunEnable(PRCM_PERIPH_TIMER1);
+   PRCMLoadSet();
+   while ( !PRCMLoadGet() );
+   TimerDisable(GPT1_BASE,TIMER_A);
+   TimerConfigure(GPT1_BASE, TIMER_CFG_PERIODIC_UP);
+   TimerEnable(GPT1_BASE ,TIMER_A);
+}
+
+void SetTimerState(int timer_state_command)
+{
+    if(timer_state_command)
+    {
+        TimerDisable(GPT1_BASE,TIMER_A);
+    }
+    else
+    {
+        TimerEnable(GPT1_BASE,TIMER_A);
+    }
+
+}
+
+uint32_t GetTime()
+{
+    return TimerValueGet(GPT1_BASE ,TIMER_A);
+}
--- cc1310_code/helpful.h
+++ cc1310_code/helpful.h
@@ -9,9 +9,16 @@
 #define HELPFUL_H_
 
 #include <stdint.h>
+#include <devices/cc13x0/driverlib/timer.h>
+#include <devices/cc13x0/driverlib/prcm.h>
+
+#define TIMER_ON 0
+#define TIMER_OFF 1
+
 
 void delay(float sec);
-
-
+void EnableTimer(void);
+void SetTimerState(int timer_state_command);
+uint32_t GetTime(void);
 
 #endif /* HELPFUL_H_ */
