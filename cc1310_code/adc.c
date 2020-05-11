e31c7d20992b7933ca594aa4f13fc15c96bee4de
JCohner <joshuacohen2019@u.northwestern.edu>
2020-05-10 21:04:18 -0500

    upped to 6 sensors for better turning

--- cc1310_code/adc.c
+++ cc1310_code/adc.c
@@ -8,6 +8,8 @@
 #include "uart.h"
 #include "helpful.h"
 
+//#define NUM_SENSORS 6
+
 
 char buffer[50];
 
@@ -17,26 +19,7 @@ void InitADC()
     while(!(AONWUCPowerStatusGet() & AONWUC_AUX_POWER_ON));
 
     AUXWUCPowerCtrl(AUX_WUC_POWER_ACTIVE);
-    //eneable interface and adc clock
-//    AUXWUCClockEnable(AUX_WUC_ADC_CLOCK | AUX_WUC_ANAIF_CLOCK | AUX_WUC_ADI_CLOCK);
-//    //wait for request ack
-//    while (AUXWUCClockStatus(AUX_WUC_ADC_CLOCK) != AUX_WUC_CLOCK_READY);
-//
-//    AUXADCSelectInput(ADC_COMPB_IN_AUXIO7); //CORRESPONDS TO DIO PIN 23
-//    WriteUART0("sizzurp\r\n");
-//    //set input pins
-////    IOCPinTypeAux(IOID_25); //TODO: figure out how to map pins better
-//    AUXADCDisable();
-////    WriteUART0("rock chalk?\r\n");
-//    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_2P7_US, AUXADC_TRIGGER_MANUAL);
-//    AUXADCGenManualTrigger();
-////    WriteUART0("killed jayhawk?\r\n");
-//    uint16_t sing_samp = AUXADCReadFifo();
-//    sprintf(buffer, "adc val: %u\r\n", sing_samp);
-//    WriteUART0(buffer);
-////    //configure ADC registers
-//    AUXADCDisable();
-//    AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_5P3_US, AUXADC_TRIGGER_MANUAL);
+
 }
 
 //while this is generically named its function is to know read the refelectance sensor array from pololu
@@ -49,7 +32,7 @@ void ReadADC(uint32_t * vals)
     AUXADCDisable();
     AUXADCEnableSync(AUXADC_REF_FIXED, AUXADC_SAMPLE_TIME_2P7_US, AUXADC_TRIGGER_MANUAL);
     int i;
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < NUM_SENSORS; i++)
     {
 //        SetAndWritePinHigh(adc_outputs[i]);
         IOCPinTypeAux(adc_outputs[i]); //tri-state the pin to switch it off
@@ -58,6 +41,7 @@ void ReadADC(uint32_t * vals)
         AUXADCGenManualTrigger();
         vals[i]= AUXADCReadFifo();
     }
+
     AUXADCDisable();
     return;
 }
