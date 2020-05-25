bf08696259cbb3d096fce7c8e7670b59d55eb8b2
JCohner <joshuacohen2019@u.northwestern.edu>
2020-05-25 18:02:54 -0500

    named IR sensing pins

--- cc1310_code/adc.h
+++ cc1310_code/adc.h
@@ -16,6 +16,13 @@
 
 #define NUM_SENSORS 6
 
+#define MR_Sense IOID_25
+#define IR_Sense IOID_23
+#define IL_Sense IOID_24
+#define ML_Sense IOID_26
+#define ER_Sense IOID_27
+#define EL_Sense IOID_28
+
 //wire it such that this is true
 static char adc_inputs[8] = {ADC_COMPB_IN_AUXIO7, ADC_COMPB_IN_AUXIO6,ADC_COMPB_IN_AUXIO5, ADC_COMPB_IN_AUXIO4, ADC_COMPB_IN_AUXIO3, ADC_COMPB_IN_AUXIO2, ADC_COMPB_IN_AUXIO1, ADC_COMPB_IN_AUXIO0};
 static char adc_outputs[8] = {IR_Sense, IL_Sense, MR_Sense, ML_Sense, ER_Sense, EL_Sense, IOID_29, IOID_30};
