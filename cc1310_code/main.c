a8044b4df49aa7a07960237e685e3b8388098247
JCohner <joshuacohen2019@u.northwestern.edu>
2020-06-21 16:36:23 -0500

    playing with intersection state machine management and openloop control

--- cc1310_code/main.c
+++ cc1310_code/main.c
@@ -21,7 +21,7 @@
 
 static char buffer[60];
 uint32_t adc_vals[8];
-
+uint32_t adc_ave_vals[8];
 int main(void)
 {
 
@@ -44,17 +44,28 @@ int main(void)
 
     //start up neccesarries for ir sensing
     IR_SenseSetup();
-//    calibrate_line(); //not sure of utility yet
-//
-//    while(1);
+//    char data;
+//    while(1){
+//        //pause until character entered
+//        data = UARTCharGet(UART0_BASE);
+//        ReadIR(adc_vals);
+//        float IR_val = read_line(adc_vals);
+//        drive_line(IR_val, adc_vals);
+//        WriteUART0("collecting...\r\n");
+//        calibrate_line(10); //not sure of utility yet
+//        setMotor(M2, 0, 0);
+//        setMotor(M1, 0, 0);
+//    }
+
+
+
     //light green LED to show setup is done
     GPIO_setDio(CC1310_LAUNCHXL_PIN_GLED);
 
 //
 //      callibrate();
 //      GPIO_setDio(CC1310_LAUNCHXL_PIN_RLED);
-      uint32_t curr_time = 0;
-      uint32_t prev_time = 0;
+
       //returns frequency of system clock: 48 MHz rn
       uint32_t freq = SysCtrlClockGet();
       sprintf(buffer, "freq: %u\r\n", freq);
@@ -64,6 +75,19 @@ int main(void)
       int counter = 0;
 
       uint32_t since_last=0;
+
+      state_track.policy = 0b10;
+
+//      while(1)
+//      {
+//          GPIO_toggleDio(BLED0);
+//          GPIO_toggleDio(BLED1);
+//          GPIO_toggleDio(BLED2);
+//          GPIO_toggleDio(BLED3);
+//          delay(.1);
+//      }
+
+
 
       while(1)
       {
@@ -83,24 +107,75 @@ int main(void)
 //          read_imu();
 //          rf_main();
 
-//          ReadADC(adc_vals);
           ReadIR(adc_vals);
-          if(since_last)
+//          if(since_last)
+//          {
+//              sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_ave_vals[5], adc_ave_vals[3], adc_ave_vals[1],
+//                                                                  adc_ave_vals[0], adc_ave_vals[2], adc_ave_vals[4]);
+//              WriteUART0(buffer);
+//              memset(adc_ave_vals, 0, 6 * 4);
+//              since_last = 0;
+//          }
+
+          if(1)
           {
-              sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1], adc_vals[0], adc_vals[2], adc_vals[4]);
+              //ODD IS LEFT
+              sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
+                                                                                adc_vals[0], adc_vals[2], adc_vals[4]);
               WriteUART0(buffer);
-              since_last = 0;
           }
+//          else
+//          {
+//              int j;
+//              for (j = 0; j < 6; j++)
+//              {
+//                  adc_ave_vals[j] += .1 * adc_vals[j];
+//              }
+//          }
 
 
 //          driver(adc_vals);
           float IR_val = read_line(adc_vals);
-//          sprintf(buffer, "IR VAL: %f\r\n", IR_val);
-//          WriteUART0(buffer);
+          sprintf(buffer, "IR VAL: %f\r\n", IR_val);
+          WriteUART0(buffer);
           drive_line(IR_val, adc_vals);
-//          sprintf(buffer, "by cocks: %f\r\n", IR_val);
-//          WriteUART0(buffer);
+          detect_poi(adc_vals);
+          sprintf(buffer, "flag val: %u\r\n", state_track.xc_flags);
+          WriteUART0(buffer);
+          if (state_track.xc_flags == 0b01)
+          {
+              if (state_track.policy & 0b01) //mask 1st bit
+              {
+                  //turn right
+              }
+              else
+              {
+                  //turn left
+                  WriteUART0("AEJFBIFBIU\r\n");
+                  GPIO_toggleDio(BLED1);
+              }
+          }
+          else if (state_track.xc_flags == 0b10)
+          {
+              if (state_track.policy & 0b10) //mask 2nd bit
+              {
+                  //turn right
+                  GPIO_toggleDio(BLED2);
+                  WriteUART0("BDIUBIFDB\r\n");
+              }
+              else
+              {
+                  //turn left
+              }
+              state_track.xc_flags = 0b11 ;
+          }
 
+          else if (state_track.xc_flags == 0b11)
+          {
+              setMotor(M2, 0, 0);
+              setMotor(M1, 0, 0);
+              while(1);
+          }
 
 //          delay(.05);
 //          WriteUART0("hi you \r\n");
