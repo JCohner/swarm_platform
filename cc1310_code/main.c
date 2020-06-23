f7b2daa39ca61c5d57d7ff196f01ccc37f48fa6d
JCohner <joshuacohen2019@u.northwestern.edu>
2020-06-22 19:54:33 -0500

    working poi detect!

--- cc1310_code/main.c
+++ cc1310_code/main.c
@@ -18,6 +18,7 @@
 #include "adc.h"
 #include "helpful.h"
 #include "ir_sense.h"
+#include "zumo_moves.h"
 
 static char buffer[60];
 uint32_t adc_vals[8];
@@ -70,36 +71,22 @@ int main(void)
       uint32_t freq = SysCtrlClockGet();
       sprintf(buffer, "freq: %u\r\n", freq);
       WriteUART0(buffer);
-
+//      while(1);
 
       int counter = 0;
 
-      uint32_t since_last=0;
-
-      state_track.policy = 0b10;
-
-//      while(1)
-//      {
-//          GPIO_toggleDio(BLED0);
-//          GPIO_toggleDio(BLED1);
-//          GPIO_toggleDio(BLED2);
-//          GPIO_toggleDio(BLED3);
-//          delay(.1);
-//      }
-
-
+//      uint32_t since_last=0;
+      set_policy(0b10);
+//      test_leds();
+//      while(1);
+      init_openloop();
+      set_total_count(48000 * 2); //probably do half of this for 45 deg
 
       while(1)
       {
 
-          if(counter % 10 == 0)
-          {
-              since_last = 1;
-          }
-
 //          sprintf(buffer,"%d\r\n", counter);
 //          WriteUART0(buffer);
-//          GPIO_setDio(CC1310_LAUNCHXL_PIN_RLED);
 
           //pause until character entered
 //          char data = UARTCharGet(UART0_BASE);
@@ -108,76 +95,22 @@ int main(void)
 //          rf_main();
 
           ReadIR(adc_vals);
-//          if(since_last)
-//          {
-//              sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_ave_vals[5], adc_ave_vals[3], adc_ave_vals[1],
-//                                                                  adc_ave_vals[0], adc_ave_vals[2], adc_ave_vals[4]);
-//              WriteUART0(buffer);
-//              memset(adc_ave_vals, 0, 6 * 4);
-//              since_last = 0;
-//          }
 
-          if(1)
-          {
-              //ODD IS LEFT
-              sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
-                                                                                adc_vals[0], adc_vals[2], adc_vals[4]);
-              WriteUART0(buffer);
-          }
-//          else
-//          {
-//              int j;
-//              for (j = 0; j < 6; j++)
-//              {
-//                  adc_ave_vals[j] += .1 * adc_vals[j];
-//              }
-//          }
+          //ODD IS LEFT
+          sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
+                                                                            adc_vals[0], adc_vals[2], adc_vals[4]);
+          WriteUART0(buffer);
 
-
-//          driver(adc_vals);
           float IR_val = read_line(adc_vals);
           sprintf(buffer, "IR VAL: %f\r\n", IR_val);
           WriteUART0(buffer);
           drive_line(IR_val, adc_vals);
           detect_poi(adc_vals);
-          sprintf(buffer, "flag val: %u\r\n", state_track.xc_flags);
+          sprintf(buffer, "flag val: %u\r\n", (get_flags() & 0b1100) >> 2);
           WriteUART0(buffer);
-          if (state_track.xc_flags == 0b01)
-          {
-              if (state_track.policy & 0b01) //mask 1st bit
-              {
-                  //turn right
-              }
-              else
-              {
-                  //turn left
-                  WriteUART0("AEJFBIFBIU\r\n");
-                  GPIO_toggleDio(BLED1);
-              }
-          }
-          else if (state_track.xc_flags == 0b10)
-          {
-              if (state_track.policy & 0b10) //mask 2nd bit
-              {
-                  //turn right
-                  GPIO_toggleDio(BLED2);
-                  WriteUART0("BDIUBIFDB\r\n");
-              }
-              else
-              {
-                  //turn left
-              }
-              state_track.xc_flags = 0b11 ;
-          }
+          execute_policy();
 
-          else if (state_track.xc_flags == 0b11)
-          {
-              setMotor(M2, 0, 0);
-              setMotor(M1, 0, 0);
-              while(1);
-          }
-
-//          delay(.05);
+//          delay(.05); //WILL MESS UP COLOR DETECT THRESHOLDING
 //          WriteUART0("hi you \r\n");
           ++counter;
       }
