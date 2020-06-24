ded89df23f0146b8696d6d420ce0304d8db31b2b
JCohner <joshuacohen2019@u.northwestern.edu>
2020-06-23 19:01:52 -0500

    added actuation and return flags as well as functionality to set and return return policy

--- cc1310_code/state_track.c
+++ cc1310_code/state_track.c
@@ -12,11 +12,27 @@ static struct StateTrack state_track;
 void set_policy(uint8_t policy)
 {
     state_track.policy = policy & 0x03;
+
+    if (!((state_track.policy >> 1) ^ (state_track.policy & 0b01)))
+    {
+        // 0b00 or 0b11 policy case
+        state_track.return_policy = state_track.policy;
+    }
+    else
+    {
+        //for 0b01 and 0b10 cases return policy is opposite
+        state_track.return_policy = ~state_track.policy;
+    }
 }
 
 uint8_t get_policy()
 {
     return state_track.policy;
+}
+
+uint8_t get_return_policy()
+{
+    return state_track.return_policy;
 }
 
 void set_flags(uint8_t flags)
@@ -48,3 +64,32 @@ uint8_t get_target()
 {
     return state_track.target;
 }
+
+void set_actuation_flag(uint8_t flag)
+{
+    state_track.actuation_flag = flag;
+}
+uint8_t get_actuation_flag()
+{
+    return state_track.actuation_flag;
+}
+
+void set_return_flag(uint8_t flag)
+{
+    state_track.return_flag = flag;
+}
+
+uint8_t get_return_flag()
+{
+    return state_track.return_flag;
+}
+
+
+
+
+
+
+
+
+
+
--- cc1310_code/state_track.h
+++ cc1310_code/state_track.h
@@ -19,22 +19,20 @@
 struct StateTrack{
     //policy and target bits
     uint8_t policy : 2;
+    uint8_t return_policy : 2;
     uint8_t target : 1;
 
     //intersection management bits
-
-    //state dia:
-    // 0b00 - no detect
-    // 0b01 - detect 1
-    // 0b10 - detect 2
-    // 0b00 - reset
     uint8_t flags : 2;
     uint8_t prev_flags : 2;
-    uint8_t err_flag : 1;
+    uint8_t actuation_flag : 1;
+    uint8_t return_flag : 1;
+//    uint8_t first_send : 1;
 };
 
 void set_policy(uint8_t policy);
 uint8_t get_policy();
+uint8_t get_return_policy();
 
 void set_flags(uint8_t flags);
 uint8_t get_flags();
@@ -44,6 +42,12 @@ uint8_t get_prev_flags();
 
 void set_target(uint8_t target);
 uint8_t get_target();
+
+void set_actuation_flag(uint8_t flag);
+uint8_t get_actuation_flag();
+
+void set_return_flag(uint8_t flag);
+uint8_t get_return_flag();
 
 //void update_state(struct ColorTrack *left, struct ColorTrack *right);
 
