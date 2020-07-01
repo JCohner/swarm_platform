802bed5c3ec698e9375b89a573f9d10f47a83e12
JCohner <joshuacohen2019@u.northwestern.edu>
2020-06-30 21:27:36 -0500

    added graphite

--- cc1310_code/color_track.c
+++ cc1310_code/color_track.c
@@ -9,13 +9,9 @@
 #include "uart.h"
 #define NUM_COLORS 3
 
-//static struct ColorTrack graphite = {.low_bound = GREY_LOW, .high_bound = GREY_HIGH, .idx = 0,
-//                                     .curr_state = 0, .accum = 0, .stash_val = 0, .detect_thresh = 7};
+static struct ColorTrack graphite = {.low_bound = GREY_LOW, .high_bound = GREY_HIGH, .detect_thresh = 5};
 
 static struct ColorTrack purp = {.low_bound = PURP_LOW, .high_bound = PURP_HIGH, .detect_thresh = 7};
-
-//static struct ColorTrack purp_right = {.low_bound = PURP_LOW, .high_bound = PURP_HIGH, .idx = 0,
-//                                       .curr_state = 0, .accum = 0, .stash_val = 0, .detect_thresh = 5};
 
 char buffer[50];
 void detect_poi(uint32_t * vals)
@@ -30,36 +26,64 @@ void detect_poi(uint32_t * vals)
     {
         purp.right_stash_val = 1;
     }
-//    graphite.prev_vals[graphite.idx] = graphite.stash_val;
+
+    if (vals[3] > graphite.low_bound && vals[3] < graphite.high_bound && vals[5] > graphite.low_bound + 20 && vals[5] < graphite.high_bound + 20)
+    {
+        graphite.left_stash_val = 1;
+    }
+    if (vals[2] > graphite.low_bound && vals[2] < graphite.high_bound && vals[4] > graphite.low_bound + 20 && vals[4] < graphite.high_bound + 20)
+    {
+        graphite.right_stash_val = 1;
+    }
+    graphite.left_prev_vals[graphite.idx] = graphite.left_stash_val;
+    graphite.right_prev_vals[graphite.idx] = graphite.right_stash_val;
+
     purp.left_prev_vals[purp.idx] = purp.left_stash_val;
     purp.right_prev_vals[purp.idx] = purp.right_stash_val;
-//    purp_right.prev_vals[purp_right.idx] = purp_right.stash_val;
+
     int i;
     for (i = 0; i < NUM_PREV_VALS; i++)
     {
-//        graphite.prev_vals_ave += graphite.prev_vals[i];
+        graphite.left_prev_vals_ave += graphite.left_prev_vals[i];
+        graphite.right_prev_vals_ave += graphite.right_prev_vals[i];
 
         purp.left_prev_vals_ave += purp.left_prev_vals[i];
         purp.right_prev_vals_ave += purp.right_prev_vals[i];
     }
 
     purp.idx = (purp.idx + 1) % NUM_PREV_VALS;
-    //    graphite.idx = (graphite.idx + 1) % NUM_PREV_VALS;
+    graphite.idx = (graphite.idx + 1) % NUM_PREV_VALS;
 
-    sprintf(buffer, "prev vals ave: %u, %u\r\n", purp.left_prev_vals_ave, purp.right_prev_vals_ave);
+    sprintf(buffer, "purp prev vals ave: %u, %u\r\n", purp.left_prev_vals_ave, purp.right_prev_vals_ave);
+    WriteUART0(buffer);
+    sprintf(buffer, "graph prev vals ave: %u, %u\r\n", graphite.left_prev_vals_ave, graphite.right_prev_vals_ave);
     WriteUART0(buffer);
 
-    if ((purp.left_prev_vals_ave >= purp.detect_thresh ||
-            purp.right_prev_vals_ave >= purp.detect_thresh) && !get_detect_flag())
+
+    if (!get_detect_flag())
     {
-        set_detect_flag(1);
-        GPIO_toggleDio(BLED0);
+        if ((purp.left_prev_vals_ave >= purp.detect_thresh ||
+            purp.right_prev_vals_ave >= purp.detect_thresh))
+        {
+            set_detect_flag(1);
+            GPIO_toggleDio(BLED0);
+        }
+        else if ((graphite.left_prev_vals_ave >= graphite.detect_thresh ||
+            graphite.right_prev_vals_ave >= graphite.detect_thresh))
+        {
+            set_detect_flag(2);
+            GPIO_toggleDio(BLED1);
+        }
+        else
+        {
+            set_detect_flag(0);
+        }
+
     }
     else
     {
         set_detect_flag(0);
     }
-
     return;
 }
 
@@ -71,10 +95,19 @@ void reinit_stash_and_accum()
     purp.right_accum = 0;
     purp.right_stash_val = 0;
 
-//    graphite.accum = 0;
-//    graphite.stash_val = 0;
-//
-//    graphite.prev_vals_ave = 0;
     purp.left_prev_vals_ave = 0;
     purp.right_prev_vals_ave = 0;
+
+    graphite.left_accum = 0;
+    graphite.left_stash_val = 0;
+
+    graphite.right_accum = 0;
+    graphite.right_stash_val = 0;
+
+    graphite.left_prev_vals_ave = 0;
+    graphite.right_prev_vals_ave = 0;
+
+
+
+
 }
