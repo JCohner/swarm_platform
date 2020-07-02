dc99c8ff59820ea520485c50d4fb855e6bf173e0
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-01 22:38:05 -0500

    rewrote detect poi logic: no need for perfect reads now

--- cc1310_code/color_track.c
+++ cc1310_code/color_track.c
@@ -11,35 +11,26 @@
 
 static struct ColorTrack graphite = {.low_bound = GREY_LOW, .high_bound = GREY_HIGH, .detect_thresh = 3};
 
-static struct ColorTrack purp = {.low_bound = PURP_LOW, .high_bound = PURP_HIGH, .detect_thresh = 6};
+static struct ColorTrack purp = {.low_bound = PURP_LOW, .high_bound = PURP_HIGH, .detect_thresh = 7};
 
 char buffer[50];
 void detect_poi(uint32_t * vals)
 {
     reinit_stash_and_accum();
 
-    if (vals[3] > purp.low_bound && vals[3] < purp.high_bound && vals[5] > purp.low_bound + 20 && vals[5] < purp.high_bound + 20)
-    {
-        purp.left_stash_val = 1;
-    }
-    if (vals[2] > purp.low_bound && vals[2] < purp.high_bound && vals[4] > purp.low_bound + 20 && vals[4] < purp.high_bound + 20)
-    {
-        purp.right_stash_val = 1;
-    }
+    //if sensor evaulates within boundary increment handed thresh val
+    purp.left_stash_val = (vals[3] > purp.low_bound && vals[3] < purp.high_bound) + (vals[5] > purp.low_bound + 20 && vals[5] < purp.high_bound + 20);
+    purp.right_stash_val = (vals[2] > purp.low_bound && vals[2] < purp.high_bound) + (vals[4] > purp.low_bound + 20 && vals[4] < purp.high_bound + 20);
 
-    if (vals[3] > graphite.low_bound && vals[3] < graphite.high_bound && vals[5] > graphite.low_bound + 20 && vals[5] < graphite.high_bound + 20)
-    {
-        graphite.left_stash_val = 1;
-    }
-    if (vals[2] > graphite.low_bound && vals[2] < graphite.high_bound && vals[4] > graphite.low_bound + 20 && vals[4] < graphite.high_bound + 20)
-    {
-        graphite.right_stash_val = 1;
-    }
-    graphite.left_prev_vals[graphite.idx] = graphite.left_stash_val;
-    graphite.right_prev_vals[graphite.idx] = graphite.right_stash_val;
+    graphite.left_stash_val = (vals[3] > graphite.low_bound && vals[3] < graphite.high_bound) +  (vals[5] > graphite.low_bound + 20 && vals[5] < graphite.high_bound + 20);
+    graphite.right_stash_val = (vals[2] > graphite.low_bound && vals[2] < graphite.high_bound) + (vals[4] > graphite.low_bound + 20 && vals[4] < graphite.high_bound + 20);
 
+    //set the current iteration of the moving average to stash val
     purp.left_prev_vals[purp.idx] = purp.left_stash_val;
     purp.right_prev_vals[purp.idx] = purp.right_stash_val;
+
+    graphite.left_prev_vals[graphite.idx] = graphite.left_stash_val;
+    graphite.right_prev_vals[graphite.idx] = graphite.right_stash_val;
 
     int i;
     for (i = 0; i < NUM_PREV_VALS; i++)
@@ -62,12 +53,12 @@ void detect_poi(uint32_t * vals)
 
     if (!get_detect_flag())
     {
-        if (purp.left_prev_vals_ave + purp.right_prev_vals_ave > purp.detect_thresh)
+        if (purp.left_prev_vals_ave > purp.detect_thresh || purp.right_prev_vals_ave > purp.detect_thresh)
         {
             set_detect_flag(1);
             GPIO_toggleDio(BLED0);
         }
-        else if (graphite.left_prev_vals_ave + graphite.right_prev_vals_ave > graphite.detect_thresh)
+        if (graphite.left_prev_vals_ave + graphite.right_prev_vals_ave > graphite.detect_thresh)
         {
             set_detect_flag(2);
             GPIO_toggleDio(BLED1);
