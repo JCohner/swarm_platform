918af649275909489a0a777126c15bf8b6fa52e6
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-03 21:19:44 -0500

    now calling second generation of functions in main loop

--- cc1310_code/main.c
+++ cc1310_code/main.c
@@ -19,6 +19,7 @@
 #include "helpful.h"
 #include "ir_sense.h"
 #include "zumo_moves.h"
+#include "color_track.h"
 
 static char buffer[60];
 uint32_t adc_vals[8];
@@ -70,13 +71,21 @@ int main(void)
 
       //sets open loop control characteristics
       set_on_time(10);
-      set_offset_time(15);
+      set_offset_time(10);
+//      init_openloop();
+//      while(1)
+//      {
+//          openloop_turn();
+//      }
+
 
       //sets inital policy pursued by robot
       set_policy(0b10);
 
       while(1)
       {
+          GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED); //pin6
+
 
 //          read_imu();
           rf_main();
@@ -84,21 +93,24 @@ int main(void)
           ReadIR(adc_vals);
 
           //ODD IS LEFT
-//          sprintf(buffer,"adc: %u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
+//          sprintf(buffer,"%u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
 //                            adc_vals[0], adc_vals[2], adc_vals[4]);
 //          WriteUART0(buffer);
-          int offset = sprintf(buffer, "stator: %u\r\n", get_return_flag() << 2 | get_xc_state());
-          offset += sprintf(buffer + offset, "prev_stator: %u\r\n", get_prev_return_flag() << 2 | get_prev_xc_state());
-          offset += sprintf(buffer + offset, "act: %u\r\n", get_actuation_flag());
-          WriteRF(buffer);
+//          int offset = sprintf(buffer, "stator: %u\r\n", get_return_flag() << 2 | get_xc_state());
+//          offset += sprintf(buffer + offset, "prev_stator: %u\r\n", get_prev_return_flag() << 2 | get_prev_xc_state());
+//          offset += sprintf(buffer + offset, "act: %u\r\n", get_actuation_flag());
+//          WriteRF(buffer);
 
           float IR_val = read_line(adc_vals);
 //          sprintf(buffer, "IR VAL: %f\r\n", IR_val);
 //          WriteUART0(buffer);
           drive_line(IR_val, adc_vals);
-          detect_poi(adc_vals);
-          evaluate_state();
-          execute_policy();
+//          detect_poi(adc_vals);
+          detect_xc(adc_vals);
+          //evaluate_state();
+          inc_state(); //if at intersection
+          //execute_policy();
+          manage_intersection();
           openloop_turn();
 
       }
