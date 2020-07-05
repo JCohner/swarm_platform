2ddb653d2b60a9aa6af89b5db0f25c3367cf6aac
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-05 18:17:41 -0500

    added color tracking of grey back such that our state transitions are more robust

--- cc1310_code/color_track.c
+++ cc1310_code/color_track.c
@@ -10,7 +10,7 @@
 
 #define NUM_COLORS 3
 
-//static struct ColorTrack graphite = {.low_bound = GREY_LOW, .high_bound = GREY_HIGH, .detect_thresh = 4};
+static struct ColorTrack graphite = {.low_bound = GREY_LOW, .high_bound = GREY_HIGH, .detect_thresh = 4};
 
 //static struct ColorTrack purp = {.low_bound = PURP_LOW, .high_bound = PURP_HIGH, .detect_thresh = 7};
 
@@ -27,7 +27,49 @@ void detect_xc(uint32_t * vals)
     return;
 }
 
+void detect_poi(uint32_t * vals)
+{
+    graphite.left_accum = 0;
+    graphite.left_stash_val = 0;
 
+    graphite.right_accum = 0;
+    graphite.right_stash_val = 0;
+
+    graphite.left_prev_vals_ave = 0;
+    graphite.right_prev_vals_ave = 0;
+
+    graphite.left_prev_vals[graphite.idx] =
+            (vals[3] > graphite.low_bound && vals[3] < graphite.high_bound) \
+            + (vals[5] > graphite.low_bound + 20 && vals[5] < graphite.high_bound + 20);
+    graphite.right_prev_vals[graphite.idx] =
+            (vals[2] > graphite.low_bound && vals[2] < graphite.high_bound) \
+            + (vals[4] > graphite.low_bound + 20 && vals[4] < graphite.high_bound + 20);
+
+
+    int i;
+    for (i = 0; i < NUM_PREV_VALS; i++)
+    {
+        graphite.left_prev_vals_ave += graphite.left_prev_vals[i];
+        graphite.right_prev_vals_ave += graphite.right_prev_vals[i];
+    }
+
+    graphite.idx = (graphite.idx + 1) % NUM_PREV_VALS;
+
+    if (!get_detect_flag() && !get_actuation_flag())
+    {
+        if (graphite.left_prev_vals_ave + graphite.right_prev_vals_ave > graphite.detect_thresh)
+        {
+            set_detect_flag(1);
+            GPIO_toggleDio(BLED0);
+            for (i = 0; i < NUM_PREV_VALS; i++)
+            {
+                graphite.left_prev_vals[i] = 0;
+                graphite.right_prev_vals[i] = 0;
+            }
+        }
+    }
+    return;
+}
 
 //void detect_poi(uint32_t * vals)
 //{
