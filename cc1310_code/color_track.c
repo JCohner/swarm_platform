a6fabe8a87deafac668097ec486d671817ba8e71
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-12 23:05:39 -0500

    fine tuning and code cleanup, performance is great. improvements from here on out will involve adding info to the map for error catching

--- cc1310_code/color_track.c
+++ cc1310_code/color_track.c
@@ -19,66 +19,13 @@ char buffer[50];
 void detect_xc(uint32_t * vals)
 {
 //    //check for white traingle
-    if (vals[0] < 500 && vals[1] < 500 && vals[2] < 500 && vals[3] < 500)
+    if (vals[0] < 500 && vals[1] < 500 && vals[2] < 500 && vals[3] < 500
+            && vals[4] < 500 && vals[5] < 500)
     {
         GPIO_toggleDio(BLED1);
         set_intersection_flag(1);
     }
-//    return;
-
-//    purp.left_accum = 0;
-//    purp.left_stash_val = 0;
-
-//    purp.right_accum = 0;
-//    purp.right_stash_val = 0;
-
-
-    int i;
-    uint8_t jarm = 1;
-    for (i = 0; i < 4; i++)
-    {
-        jarm = jarm && (vals[i+1] > purp.low_bound && vals[i+1] < purp.high_bound);
-        sprintf(buffer, "jarm: %u\r\n", jarm);
-        WriteUART0(buffer);
-    }
-
-    if (jarm)
-    {
-        GPIO_toggleDio(BLED1);
-        set_intersection_flag(1);
-    }
-return;
-
-//    purp.left_prev_vals_ave = 0;
-////    purp.right_prev_vals_ave = 0;
-//
-//
-//    //just using center val
-//    purp.left_prev_vals[purp.idx] =
-//            (vals[2] > purp.low_bound && vals[2] < purp.high_bound) \
-//            + (vals[3] > purp.low_bound && vals[3] < purp.high_bound);
-//
-//
-//    int i;
-//    for (i = 0; i < NUM_PREV_VALS; i++)
-//    {
-//        purp.left_prev_vals_ave += purp.left_prev_vals[i];
-//    }
-//
-//    purp.idx = (purp.idx + 1) % NUM_PREV_VALS;
-//
-//    if(purp.left_prev_vals_ave > purp.detect_thresh)
-//    {
-//        GPIO_toggleDio(BLED1);
-//        set_intersection_flag(1);
-//        for (i = 0; i < NUM_PREV_VALS; i++)
-//        {
-//            purp.left_prev_vals[i] = 0;
-//        }
-//    }
-
     return;
-
 }
 
 void detect_poi(uint32_t * vals)
@@ -121,6 +68,14 @@ void detect_poi(uint32_t * vals)
                 graphite.right_prev_vals[i] = 0;
             }
         }
+    }
+
+
+    if (graphite.left_prev_vals_ave > 7 && graphite.right_prev_vals_ave < 2
+            && (get_return_flag() << 2 |get_xc_state())== 0b010)
+    {
+        set_target_flag(1);
+        GPIO_toggleDio(BLED2);
     }
     return;
 }
