fbd502e34350a6f8ad1eff1f1b68c8e25332f782
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-13 18:18:40 -0500

    ironed out last bugs that would let me use this at 1.27ms period

--- cc1310_code/interrupt_timer.c
+++ cc1310_code/interrupt_timer.c
@@ -8,12 +8,14 @@
 #include <interrupt_timer.h>
 
 #include "gpio.h"
+#include "zumo_moves.h"
 
 void TimerInt()
 {
     GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
-    //documentation recomends this isn't the very last function call
     TimerIntClear(GPT1_BASE, TIMER_TIMA_TIMEOUT);
+
+
 }
 
 void InterTimerEnable()
@@ -26,21 +28,20 @@ void InterTimerEnable()
 
     TimerDisable(GPT1_BASE,TIMER_A);
 
-    TimerConfigure(GPT1_BASE, TIMER_CFG_A_PERIODIC);
-
-    TimerPrescaleSet(GPT1_BASE, TIMER_A, 0xFF);
+    TimerConfigure(GPT1_BASE, (TIMER_CFG_A_PERIODIC | TIMER_CFG_B_PERIODIC)); //this breaks it
+//
+    TimerPrescaleSet(GPT1_BASE, TIMER_A, 0xFF); // this doesnt work because it thinks its in 32 bit mode
     TimerPrescaleMatchSet(GPT1_BASE, TIMER_A, 0xFF);
-    TimerLoadSet(GPT1_BASE, TIMER_A, 0xFFFF); // Period = 255 * 65535 / freq = 0.25 sec
-    TimerMatchSet(GPT1_BASE, TIMER_A, 0xFFFF / 2); // PulseWidth = Period / 2 = 0.125 sec
+    TimerLoadSet(GPT1_BASE, TIMER_A, 0xFFFF);
+    TimerMatchSet(GPT1_BASE, TIMER_A, 0xFFFF / 2);
 
     IntMasterEnable();
-    TimerIntRegister(GPT1_BASE, TIMER_A, TimerInt);
+    TimerIntRegister(GPT1_BASE, TIMER_A, openloop_turn);
     TimerIntEnable(GPT1_BASE, TIMER_TIMA_TIMEOUT);
     IntEnable(INT_GPT1A);
-    TimerLoadSet(GPT1_BASE,TIMER_A,  48e6);
+//    TimerLoadSet(GPT1_BASE,TIMER_A,  48e6 / 2); //this line is needed think harder bout it
 
     TimerEnable(GPT1_BASE,TIMER_A);
-
 }
 
 
