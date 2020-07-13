01199411fa590fe4c1c953fd679398f014ea80db
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-13 15:38:13 -0500

    moved header-like stuff here to clean up zumo_rf.c. constants such as PAYLOAD_LENGTH, TX_DELAY, IDLE_MAX, and such set here. same with some of the less pertinent globals

--- cc1310_code/zumo_rf.h
+++ cc1310_code/zumo_rf.h
@@ -8,10 +8,82 @@
 #ifndef ZUMO_RF_H_
 #define ZUMO_RF_H_
 
+#include <drivers/rf/RF.h>
+#include <drivers/power/PowerCC26XX.h>
+
+/* Application Header files */
+#include "RFQueue.h"
+#include "smartrf_settings/smartrf_settings.h"
+#include "application_settings.h"
+
+#include <devices/cc13x0/driverlib/gpio.h>
+#include <devices/cc13x0/driverlib/rf_common_cmd.h>
+
+#include "uart.h"
+#include "CC1310_LAUNCHXL.h"
+#include "gpio.h"
+
+//#include "state_track.h" //replaced w comm_packet
+#include "comm_packet.h"
+
+#include <stdlib.h>
+
+/***** Defines *****/
+#define PAYLOAD_LENGTH          50
+#define PACKET_INTERVAL_US      200000
+/* Number of times the CS command should run when the channel is BUSY */
+#define CS_RETRIES_WHEN_BUSY    100
+/* The channel is reported BUSY is the RSSI is above this threshold */
+#define RSSI_THRESHOLD_DBM      -80
+#define IDLE_TIME_US            5000
+/* Proprietary Radio Operation Status Codes Number: Operation ended normally */
+#define PROP_DONE_OK            0x3400
+#define PROP_DONE_IDLE          0x3407
+#define PROP_DONE_BUSY          0x3408 //from rf_prop_mailbox
+#define PROP_DONE_IDLETIMEOUT   0x3409  ///< Carrier sense operation ended because of timeout with csConf.timeoutRes = 1
+#define PROP_DONE_BUSYTIMEOUT   0x340A
+#define TX_DELAY             (uint32_t)(4000000*0.1f) //TODO: add this delay
+#define IDLE_MAX 30
+#define DELTA_TIME_BUFF_SIZE 10
+
+/* NOTE: Only two data entries supported at the moment */
+#define NUM_DATA_ENTRIES       2
+/* The Data Entries data field will contain:
+ * 1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
+ * Max 30 payload bytes
+ * 1 status byte (RF_cmdPropRx.rxConf.bAppendStatus = 0x1) */
+#define NUM_APPENDED_BYTES     2
+
+/***** Variable declarations *****/
+static RF_Object rfObject;
+static RF_Handle rfHandle;
+
+static uint8_t packet[PAYLOAD_LENGTH];
+static uint16_t seqNumber;
+
+static uint32_t time;
+
+/* Receive Statistics */
+static rfc_propRxOutput_t rxStatistics;
+
+/* Receive dataQueue for RF Core to fill in data */
+static dataQueue_t dataQueue;
+static rfc_dataEntryGeneral_t* currentDataEntry;
+static uint8_t packetLength;
+static uint8_t* packetDataPointer;
+
+#if defined(__TI_COMPILER_VERSION__)
+#pragma DATA_ALIGN(rxDataEntryBuffer, 4)
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  PAYLOAD_LENGTH,
+                                                  NUM_APPENDED_BYTES)];
+#endif //defined(__TI_COMPILER_VERSION__)
+
 void rf_setup();
 void rf_main();
 
 void WriteRF(const char * string);
 void WriteRFState(uint8_t state);
-
+void WriteRFPacket(uint8_t comm_packet);
 #endif /* ZUMO_RF_H_ */
