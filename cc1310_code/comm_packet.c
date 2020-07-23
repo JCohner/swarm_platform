bef9f77ee4d9ca0a5323a84a26b8b0a33975eb61
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-23 18:19:52 -0500

    added better logic to check near, it is a little opaque, but should be discernable if you track the bitwise operators

--- cc1310_code/comm_packet.c
+++ cc1310_code/comm_packet.c
@@ -44,13 +44,75 @@ void evaluate_packet(uint8_t packet)
 //checks if sender robot is near enough to communicate
 uint8_t check_near(struct Packet * info)
 {
-    //check the handedness of the robots path
-    if ((info->policy & 0b01) ==(get_policy() & 0b01))
+    uint8_t match_first_pol_bit = ((info->policy & 0b01) == (get_policy() & 0b01));
+
+    switch(info->xc_state ^ get_xc_state())
     {
+        case(0b000):
+                //x!=2 ^ x!=2 - clear to send
+                //2 ^ 2 - must check first policy bit
 
+                //both CTS if pol matches
+                if (match_first_pol_bit)
+                {
+                    return 1;
+                }
+                //if not 2 CTS
+                else if (info->xc_state != 0b010)
+                {
+                    return 1;
+                }
+                //NOT CTS if both at 2 but do not share first pol bit
+                else
+                {
+                    return 0;
+                }
+
+                break;
+        case(0b011):
+                // 1 ^ 2 - must check first policy bit
+                // 5 ^ 6 - clear to send
+
+                //both CTS if pol matches
+                if (match_first_pol_bit)
+                {
+                    return 1;
+                }
+                //if info is 5 | 6 CTS
+                else if (info->xc_state & 0b100)
+                {
+                    return 1;
+                }
+                //NOT CTS - if MSB != 1 (as above) must be 1 ^ 2 with matching LSB pol
+                else
+                {
+                    return 0;
+                }
+
+                break;
+        case(0b100):
+                // 1 ^ 5 - NOT clear to send
+                // 2 ^ 6 - NOT clear to send
+                return 0;
+                break;
+        case(0b111):
+                //1 ^ 6 - clear to send
+                //2 ^ 5 - must check first policy bit
+                if (match_first_pol_bit)
+                {
+                    return 1;
+                }
+                // 6 - 1 is 5 and CTS
+                else if ((info->xc_state - get_xc_state()) == 5)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+                break;
+        default:
+            while(1);
     }
-
-
-
-
 }
