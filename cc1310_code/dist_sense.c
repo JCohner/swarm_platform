6df8a6e47c743566f59e91e6e4c15f8b374e8855
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-24 13:46:48 -0500

    code for interfacing w analog distance sensor

---
+++ cc1310_code/dist_sense.c
@@ -0,0 +1,24 @@
+/*
+ * dist_sense.c
+ *
+ *  Created on: Jul 23, 2020
+ *      Author: jambox
+ */
+
+#include "dist_sense.h"
+#include "uart.h"
+#include <stdio.h>
+
+
+static char buffer[50];
+float ReadDist()
+{
+    uint32_t analog_sig = Read1ADC(7);
+
+    sprintf(buffer, "sig: %u\r\n", analog_sig);
+    WriteUART0(buffer);
+
+    float dist = analog_sig * 2;
+
+    return dist;
+}
---
+++ cc1310_code/dist_sense.h
@@ -0,0 +1,17 @@
+/*
+ * dist_sense.h
+ *
+ *  Created on: Jul 23, 2020
+ *      Author: jambox
+ */
+
+#ifndef DIST_SENSE_H_
+#define DIST_SENSE_H_
+
+#include "adc.h"
+#include <stdint.h>
+
+float ReadDist();
+
+
+#endif /* DIST_SENSE_H_ */
