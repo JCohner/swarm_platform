628d096b2d0301250ee7f07332b26d0f6fad285c
JCohner <joshuacohen2019@u.northwestern.edu>
2020-07-28 18:06:15 -0500

    now oscillates between branches as well as within!

--- scripts/ohe_state_trans.c
+++ scripts/ohe_state_trans.c
@@ -3,25 +3,39 @@
 
 uint8_t mask_mask(uint8_t bb)
 {
-	switch(bb)
+
+	uint8_t mask_mask = 0;
+	for (int i = bb - 1; i >= 0; i--)
 	{
-		case 3:
-		return 0x7;
-		break;
-		case 4:
-		return 0xF;
-		break;
+		mask_mask |= 0b1 << i;
+		// printf("mask: %X\r\n", mask);
 	}
+
+	return mask_mask;
 }
 
+
+uint8_t get_mask(uint8_t bb)
+{
+	uint8_t mask = ~(((0b1 << (bb - 1)))) & mask_mask(bb);
+	return mask;
+}
+
+
+#define NUM_BRANCHS 2;
 
 int main()
 {
 	uint8_t xcs = 0b001;
-	uint8_t bb = 3;
+	uint8_t prev_xcs = xcs;
+
+	uint8_t bbs[2] = {3,4};
+	uint8_t bb_idx = 0;
+
+	uint8_t bb = bbs[bb_idx];
 	uint8_t ret = 0;
 
-	uint8_t mask = ~(((0b1 << (bb - 1)))) & mask_mask(bb);
+	uint8_t mask = get_mask(bb);
 	printf("mask: %X\r\n", mask);
 	for (int i = 0; i < 12; i++)
 	{
@@ -30,7 +44,13 @@ int main()
 		xcs = xcs % mask;
 		xcs = xcs | ret << (bb - 1);
 		ret = (((xcs & (0b1 << (bb - 2)))) >> (bb - 2)) ^ ret;
-		// printf("ret: %X\r\n", ret);
+		if (xcs < prev_xcs)
+		{
+			bb_idx = (bb_idx + 1) % NUM_BRANCHS;
+			bb = bbs[bb_idx];
+			mask = get_mask(bb);
+		}
 
+		prev_xcs = xcs;
 	}
 }
