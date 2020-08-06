74b9ba2e56c4d1c4388d430f4ec0e7cfd59fad47
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-05 19:49:09 -0500

    api to post messages

--- listener/rf_listener.h
+++ listener/rf_listener.h
@@ -11,16 +11,5 @@
 void rf_setup();
 void rf_main();
 
-#define MACH_MASK 0x1F800
-#define POLICY_MASK 0x003E0
-#define TFLAG_MASK 0x00400
-#define BBI_MASK 0x000010
-#define STATE_MASK 0x00000F
-
-#define MACH_SHIFT 11
-#define POL_SHIFT 5
-#define TFLAG_SHIFT 10
-#define BBI_SHIFT 4
-#define STATE_SHIFT 0
-
+void rf_post_message(uint32_t data);
 #endif /* RF_LISTENER_H_ */
