1e4529f18752598ac1116dda254dd4892629119d
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-07 18:06:19 -0500

    some utility functions

---
+++ scripts/parser/helpful.py
@@ -0,0 +1,13 @@
+import sys
+def int_to_bin_str(val):
+	strong = ""
+	for i in range(4,-1,-1):
+		if val & pow(2, i):
+			strong += '1'
+		else:
+			strong += '0'
+
+	return strong
+
+if __name__ == '__main__':
+	print(int_to_bin_str(int(sys.argv[1], 16)))
