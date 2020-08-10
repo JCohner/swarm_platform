550ef4751f1ad4059640eb2bacf342e8fb6bf736
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-09 19:24:07 -0500

    connected buttons to actual functionalities

--- scripts/parser/hub.py
+++ scripts/parser/hub.py
@@ -8,11 +8,14 @@ from PyQt5.QtCore import *
 
 from data_handler import data_in
 from helpful import int_to_bin_str
+from comm_packet import packet
+
 
 class UI(QWidget):
 
 	def __init__(self):
 		super().__init__()
+		self.curr_pack = packet()
 		grid = QGridLayout()
 		self.setLayout(grid)
 
@@ -33,19 +36,29 @@ class UI(QWidget):
 		cmd_grid = QGridLayout()
 		ID_edit = QLineEdit()
 		ID_edit.setPlaceholderText("Robot ID <press Enter>")
+		ID_edit.returnPressed.connect(self.set_mach_id)
 		cmd_grid.addWidget(ID_edit, 0,0, 1, 2)
 		start_btn = QPushButton("start",self)
 		stop_btn = QPushButton("stop",self)
+
+		self.enable_btns = [start_btn, stop_btn]
+		start_btn.clicked[bool].connect(self.enable)
+		stop_btn.clicked[bool].connect(self.enable)
+
+
 		cmd_grid.addWidget(start_btn, 1,0, 1, 1)
 		cmd_grid.addWidget(stop_btn, 1, 1, 1, 1)
 		
 
 		self.ID_edit = ID_edit
 		self.policy_combo_box = QComboBox();
+		# self.policy_combo_box.setPlaceholderText("set policy")
 		for i in range(pow(2,5)):
-			self.policy_combo_box.addItem(int_to_bin_str(i))
+			self.policy_combo_box.addItem(int_to_bin_str(i, 5))
 		cmd_grid.addWidget(self.policy_combo_box,2, 0, 1, 2)
 		send_pol_btn = QPushButton("send policy")
+		send_pol_btn.clicked[bool].connect(self.upload_pol)
+
 		cmd_grid.addWidget(send_pol_btn,3, 1, 1, 1)
 		disp_info_btn = QPushButton("disp info")
 		cmd_grid.addWidget(disp_info_btn,3, 0, 1, 1)
@@ -69,6 +82,10 @@ class UI(QWidget):
 		trail_group_grid.addWidget(end_trial_btn, 2,0,1,1)
 		trail_group.setLayout(trail_group_grid)
 		grid.addWidget(trail_group, 0,1,2,1)
+
+		self.info_disp = QLabel("info:")
+		grid.addWidget(self.info_disp, 3,0,1,2)
+
 		# self.line_edit = QLineEdit()
 		# enter_line = QPushButton("Enter",self)
 		# enter_line.clicked[bool].connect(self.transmit)
@@ -87,6 +104,31 @@ class UI(QWidget):
 		self.center()
 		self.show()
 
+	def set_mach_id(self):
+		self.curr_pack.set_mach_id(self.ID_edit.text())
+		self.ID_edit.setPlaceholderText(self.ID_edit.text())
+		self.info_disp.setText("curr bot: {}".format(self.ID_edit.text()))
+		self.ID_edit.setText("")
+
+
+	def enable(self):
+		source = self.sender()
+		self.curr_pack.set_cmd("enable")
+		if (source == self.enable_btns[0]):
+			self.curr_pack.set_info(1)
+			txt = "on"
+		elif (source == self.enable_btns[1]):
+			self.curr_pack.set_info(0)
+			txt = "off"
+		self.data_sock.write_packet(self.curr_pack)
+		self.info_disp.setText("turning bot {} {}".format(self.curr_pack.mach_id, txt))
+
+	def upload_pol(self):
+		print(self.policy_combo_box.currentText())
+		self.curr_pack.set_info(self.policy_combo_box.currentText())
+		self.curr_pack.set_cmd("set_pol")
+		self.data_sock.write_packet(self.curr_pack)
+
 	def center(self):
 		qr = self.frameGeometry()
 		#print(qr)
@@ -95,14 +137,16 @@ class UI(QWidget):
 		qr.moveCenter(cp)
 		self.move(qr.topLeft())
 
-	def transmit(self):
-		source = self.sender
+
+
+	# def transmit(self):
+	# 	source = self.sender
 		
-		print(source)
-		print("{}".format(self.line_edit.text()))
+	# 	print(source)
+	# 	print("{}".format(self.line_edit.text()))
 
 
-		self.line_edit.setText("") 
+	# 	self.line_edit.setText("") 
 
 	def csv_name_click(self):
 		self.data_sock.set_csv_name(self.csv_name_edit.text())
