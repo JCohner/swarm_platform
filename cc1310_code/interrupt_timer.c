d2f9b32dfa8148fbdc6ec4adaa2ae1438eaeb415
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-10 19:06:37 -0500

    fixed up some silliniess, now properly splitting the timer (GPT1) into Timer_A and Timer_B using timerA for motor control, timerB for fixed rate actuation of radio

--- cc1310_code/interrupt_timer.c
+++ cc1310_code/interrupt_timer.c
@@ -13,7 +13,7 @@
 void TimerInt()
 {
     GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
-    TimerIntClear(GPT1_BASE, TIMER_TIMA_TIMEOUT);
+    TimerIntClear(GPT1_BASE, TIMER_TIMB_TIMEOUT);
 
 
 }
@@ -27,21 +27,34 @@ void InterTimerEnable()
     while ( !PRCMLoadGet() );
 
     TimerDisable(GPT1_BASE,TIMER_A);
+    TimerDisable(GPT1_BASE,TIMER_B);
 
-    TimerConfigure(GPT1_BASE, (TIMER_CFG_A_PERIODIC | TIMER_CFG_B_PERIODIC)); //this breaks it
-//
+
+    TimerConfigure(GPT1_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC | TIMER_CFG_B_PERIODIC)); //
     TimerPrescaleSet(GPT1_BASE, TIMER_A, 0xFF); // this doesnt work because it thinks its in 32 bit mode
-    TimerPrescaleMatchSet(GPT1_BASE, TIMER_A, 0xFF);
-    TimerLoadSet(GPT1_BASE, TIMER_A, 0xFFFF);
-    TimerMatchSet(GPT1_BASE, TIMER_A, 0xFFFF / 2);
+    TimerLoadSet(GPT1_BASE, TIMER_A, 0xFFFF / 256);
+//    TimerMatchSet(GPT1_BASE, TIMER_A, 0xFFFF / 4);
+//
+    TimerPrescaleSet(GPT1_BASE, TIMER_B, 0xFF);
+    TimerLoadSet(GPT1_BASE, TIMER_B, 0xFFFF / 512);
+//    TimerMatchSet(GPT1_BASE, TIMER_B, 0xFFFF / 2);
+
 
     IntMasterEnable();
-    TimerIntRegister(GPT1_BASE, TIMER_A, openloop_turn);
+    TimerIntRegister(GPT1_BASE, TIMER_A, openloop_turn); //
     TimerIntEnable(GPT1_BASE, TIMER_TIMA_TIMEOUT);
+    IntPrioritySet(INT_GPT1A, INT_PRI_LEVEL3);
     IntEnable(INT_GPT1A);
+//
+    TimerIntRegister(GPT1_BASE, TIMER_B, TimerInt);
+    TimerIntEnable(GPT1_BASE, TIMER_TIMB_TIMEOUT);
+    IntPrioritySet(INT_GPT1B, INT_PRI_LEVEL4);
+    IntEnable(INT_GPT1B);
+
 //    TimerLoadSet(GPT1_BASE,TIMER_A,  48e6 / 2); //this line is needed think harder bout it
 
     TimerEnable(GPT1_BASE,TIMER_A);
+    TimerEnable(GPT1_BASE,TIMER_B);
 }
 
 
