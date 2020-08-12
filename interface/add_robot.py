9a789c15b77153bf8bfd87136a0c651644a5c2a3
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-12 17:53:56 -0500

    add mem class

--- interface/add_robot.py
+++ interface/add_robot.py
@@ -5,11 +5,21 @@ import sys
 def get_idx(df, new_mac):
 	return df.index[df.loc[:,'MAC'] == new_mac][0]
 
+class MAC_mem:
+	def __init__(self):
+		self.mem = pd.read_csv('mem.csv',header = 0, index_col = 0)
+
+	def isin(self, MAC):
+		if (self.mem.isin([str(MAC)]).any().any()):
+			print("already in df! idx is: {}".format(get_idx(df, str(MAC))))
+		else:
+			print("adding new element!")
+			self.mem = pd.concat([self.mem, pd.DataFrame([str(MAC)], columns=["MAC"])], ignore_index = True, axis = 0)
 
 
 if __name__ == '__main__':
 	new_mac = sys.argv[1]
-	df = pd.read_csv('mem.csv')
+	df = pd.read_csv('mem.csv',header = 0, index_col = 0)
 	print(new_mac)
 	print(df)
 	print(df.loc[:,'MAC'].isin([new_mac]).any().any())
@@ -18,7 +28,7 @@ if __name__ == '__main__':
 	else:
 
 		print("adding new element!")
-		df.append([new_mac])
+		df = pd.concat([df, pd.DataFrame([new_mac], columns=["MAC"])], ignore_index = True, axis = 0)
 	print(df)
 
 	df.to_csv('mem.csv')
