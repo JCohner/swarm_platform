168659d12025ddaddbc88cc9ac0211b94bbe8643
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-21 19:40:03 -0500

    added side distance sensor code and some proto doxxygen comments

--- cc1310_code/interrupt_timer.c
+++ cc1310_code/interrupt_timer.c
@@ -1,12 +1,10 @@
-/*
- * interupt_timer.c
- *
- *  Created on: Jul 5, 2020
- *      Author: jambox
+/**
+ * @file interupt_timer.c
+ * @brief Contains intialization for timer based interrupts and local
+ * definitions of handler functions that act as wrappers for main functionalities
  */
 
-#include <interrupt_timer.h>
-
+#include "interrupt_timer.h"
 #include "gpio.h"
 #include "zumo_moves.h"
 #include "zumo_rf.h"
@@ -17,12 +15,23 @@
 #include "leds.h"
 #include "dist_sense.h"
 
+/**
+ * @breif Handy debug function for checking pin toggling on an oscilliscope
+ *
+ * Handy function to link with TimerIntRegister(GPT?_BASE, TIMER_?,TimerInt)
+ * to test frequency of timer based interrupt
+ * */
 void TimerInt()
 {
     GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
     TimerIntClear(GPT1_BASE, TIMER_TIMB_TIMEOUT);
 }
-
+/**
+ * @brief Wrapper for RF domain functions, called in GPT3A Interrupt
+ *
+ * Performs and iteration of the 'rf main' and packages data into the
+ * packet that will be transmitted when it is this units turn to transmit
+ */
 void pack_and_transmit()
 {
 //    GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
@@ -30,6 +39,13 @@ void pack_and_transmit()
     WriteRFPacket(get_packet());
     TimerIntClear(GPT3_BASE, TIMER_TIMA_TIMEOUT);
 }
+/**
+ * @brief main loop that reads IR values, determines position on line, reads distances sensor
+ * commands the motors such that line following and distance is maintained,
+ * detects points of interest and intersections, and manages state. Only runs if
+ * hub node broadcasts 'enable' otherwise sets motors to off.
+ */
+
 static uint32_t adc_vals[8];
 void main_loop()
 {
@@ -44,11 +60,11 @@ void main_loop()
 //                            adc_vals[0], adc_vals[2], adc_vals[4]);
 //          WriteUART0(buffer);
     float IR_val = read_line(adc_vals);
-    uint32_t Dist_val = ReadDist();
+    uint32_t forDisVal = ReadDistForward();
+    uint32_t sideDisVal = ReadDist45();
 //          sprintf(buffer, "DIST VAL: %f\r\n", Dist_val);
 //          WriteUART0(buffer);
-    drive_line(IR_val, Dist_val, adc_vals);
-
+    drive_line(IR_val, forDisVal, sideDisVal, adc_vals);
     detect_poi(adc_vals);
     detect_xc(adc_vals);
     inc_state();
@@ -64,10 +80,14 @@ void main_loop()
 }
 
 
-
+/**
+ * @brief Configures timer specific parameters and links interrupts
+ *
+ * Enables General Purpose Timeres 1 & 3 which will be used for interrupts
+ * Configures timing of the interrupts and links to handler functions
+ */
 void InterTimerEnable()
 {
-
     //enable peripheral
     PRCMPeripheralRunEnable(PRCM_PERIPH_TIMER1);
     PRCMLoadSet();
