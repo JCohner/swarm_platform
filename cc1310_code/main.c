bdc55f20f7570fd2c682efe4a45e16be935570b3
JCohner <joshuacohen2019@u.northwestern.edu>
2020-08-21 19:45:15 -0500

    added doyxgen style (and general) comments, randomizedd starting policy, added some debug prints to main loop

--- cc1310_code/main.c
+++ cc1310_code/main.c
@@ -1,14 +1,21 @@
 
 
 /**
- * main.c
+ * @file main.c
+ * @brief Main file of the project
+ *
+ * main's primary use is to perform all necessary initializations.
+ * the "main" while loop is also useful for debugging; however, all critical
+ * code is executed in timer interrupts.
  */
+
+//CC1310 includes
 #include <ccfg.c>
 #include <color_track.h>
 #include "CC1310_LAUNCHXL.h"
-
 #include <devices/cc13x0/driverlib/prcm.h>
 #include <devices/cc13x0/driverlib/sys_ctrl.h>
+//developed includes
 #include "imu.h"
 #include "gpio.h"
 #include "i2c_accel.h"
@@ -31,7 +38,7 @@ uint32_t adc_vals[8];
 uint32_t adc_ave_vals[8];
 int main(void)
 {
-
+    //CC1310 Requires you specifically turn on any peripheral you are going to use
     PRCMPowerDomainOn(PRCM_DOMAIN_PERIPH | PRCM_DOMAIN_SERIAL | PRCM_DOMAIN_RFCORE);
     while (PRCMPowerDomainStatus(PRCM_DOMAIN_PERIPH | PRCM_DOMAIN_SERIAL | PRCM_DOMAIN_RFCORE)
     != PRCM_DOMAIN_POWER_ON);
@@ -43,60 +50,75 @@ int main(void)
     PWMEnable(); //sets timers to finish configuring analog out pins
     //initialize analog input
     InitADC();
+    //initialize True Random Number Gen periph
+    RandoConfig();
+    //initializes polict and sets machine ID to first 2 bytes of MAC address
 
-    set_policy(0b11);
+
+
     set_mach_id(HWREG(FCFG1_BASE +0x2F0) & 0xFFFF);
     //configures rf driver, configures application specific packages, makes initial chirp call
     rf_setup();
-
+    //explicitly starts motors in off state
     setMotor(M2, 0, 0);
     setMotor(M1, 0, 0);
+    //usefull delay on startup (also seems needed for random number gen??)
     delay(1);
-    //start up neccesarries for ir sensing
+    set_policy(get_random_num(4));
+//    sprintf(buffer, "rand: %u\r\n" ,get_policy());
+//    WriteUART0(buffer);
+//    while(1);
+    //start up neccesarries for ir sensing (GPT2A full width set up)
     IR_SenseSetup();
-
+    //configures GPIOs associated with LEDs
     setup_leds();
 
-    //    char data;
-    ////pause until character entered
-    //data = UARTCharGet(UART0_BASE);
+    //sets open loop control characteristics
+    set_on_time(400);
+    set_offset_time(500);
 
-    //light green LED to show setup is done
-//    GPIO_setDio(CC1310_LAUNCHXL_PIN_GLED);
-//      callibrate();
-//      test_leds();
+    /*
+     * State Track Initialization for Circulatory Map
+     * exclusively comment this in for state machine
+     * to be in circulatory map configuration
+     * */
+    //uint8_t bbs[2] = {3,4};
+    //init_state(0b1100, 2, bbs, 4, 1, 0, 0x7);
 
-      //sets open loop control characteristics
-      set_on_time(325);
-      set_offset_time(500);
-
-//      //state track for circ
-//      uint8_t bbs[2] = {3,4};
-//      init_state(0b1100, 2, bbs, 4, 1, 0, 0x7);
-
-      //state track for 5 node
+    /*
+     * State Track Initialization for 5 node map
+     * exclusively comment this in for state machine
+     * to be in 5 node map configuration
+     * */
       uint8_t bbs[1] = {3};
       init_state(0b110, 1, bbs, 3, 0, 0, 0x3);
 
-
-      RandoConfig();
-//      WriteUART0("sup\r\n");
-//      sprintf(buffer, "got a rando hot off the press: %u\r\n", get_random_num(128));
-//      WriteUART0(buffer);
-//      while(1);
-
+      //Enable timer based interrupts:
+      //    GPT1A - Openloop motor control
+      //    GPT1B - Information sensing and decision making
+      //    GPT3A - RF Communication
       InterTimerEnable();
 
-      uint32_t count = 0;
       while(1)
       {
+//          uint32_t forw = ReadDistForward();
+//          uint32_t side = ReadDist45();
+//          sprintf(buffer, "%u %u\r\n", forw, side);
+//          WriteUART0(buffer);
 
-//            sprintf(buffer,"xc: %u\tpol: %u\r\n", get_xc_state(), get_policy());
-//            WriteUART0(buffer);
+
+
+//          ReadIR(adc_vals);
+//////          WriteRF(adc_vals);
+//                    sprintf(buffer,"%u, %u, %u, %u, %u, %u\r\n", adc_vals[5], adc_vals[3], adc_vals[1],
+//                                      adc_vals[0], adc_vals[2], adc_vals[4]);
+//                    WriteUART0(buffer);
+
+
+//          sprintf(buffer,"dist val %u\r\n", ReadDist());
+//          WriteUART0(buffer);
 //          GPIO_toggleDio(CC1310_LAUNCHXL_PIN_GLED);
 //          read_imu();
-//          rf_main();
-//          WriteRFPacket(get_packet());
       }
 
 
